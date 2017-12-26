5de40efc03f5e5baac18cf8ab5f8967f5d39f102
vamsi <vamsi@vamsiuzumaki>
2017-12-26 10:04:43 -0600

    Initial change

---
+++ GitExample/src/com/abc/Hello.java
@@ -0,0 +1,10 @@
+package com.abc;
+
+public class Hello {
+	
+	public String hello(String message)
+	{
+		return message;
+		
+	}
+}
---
+++ GitExample/src/com/abc/Main.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Hello H = new Hello();
+		System.out.println(H.hello("HAPPY HOLIDAYSSS123"));
+	}
+
+}
